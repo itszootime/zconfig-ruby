6f391836bd6adcfe8474a385d4df39adc1b709e8
Richard Jones <itszootime@gmail.com>
2016-03-15 16:18:05 +1300

    unit specs for setup

---
+++ spec/zconfig/setup_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe ZConfig::Setup do
+  let(:setup) { ZConfig::Setup.new }
+
+  describe "#active_path" do
+    it "returns path for current environment" do
+      setup.base_path = "/etc/zconfig"
+      setup.environment = :test
+      expect(setup.active_path).to eq("/etc/zconfig/test")
+    end
+  end
+
+  describe "#environment" do
+    it "returns environment when set" do
+      setup.environment = :production
+      expect(setup.environment).to eq(:production)
+    end
+
+    it "defaults to development" do
+      expect(setup.environment).to eq(:development)
+    end
+  end
+end
